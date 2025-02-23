598c6a42fa94c967691b855c3e795579bc60611e
aryank831 <155168497+aryank831@users.noreply.github.com>
2025-02-23 23:48:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -e  # Stop on errors
+
+# Install system dependencies
 apt-get update && apt-get install -y portaudio19-dev
+
+# Install Python dependencies
 pip install -r requirements.txt
